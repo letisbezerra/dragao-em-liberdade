e63ee2dee3bc983ed3d46b01508d71861be3f743
Leticia Bezerra <leticiabezerrauy@gmail.com>
2026-03-25 20:09:10 -0300

    updated api url

--- frontend/src/components/ChatSection.tsx
+++ frontend/src/components/ChatSection.tsx
@@ -39,7 +39,7 @@ const ChatSection = () => {
 
     // In production, this would call an edge function
     try {
-      const res = await fetch("http://127.0.0.1:8000/chat", {
+      const res = await fetch("https://dragao-em-liberdade.onrender.com/chat", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
